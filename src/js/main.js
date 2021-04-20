804a52ddbd06ca0b3c6052a2995ab6124337a291
123 <qeqw@eqwdas.com>
2021-04-20 09:37:28 +0300

    декстоп версия

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-import 'focus-visible'
+import 'focus-visible';
+import AOS from 'aos';
 import lazyImages from './modules/lazyImages';
 import scrollTo from './modules/scrollTo';
 import documentReady from './helpers/documentReady';
@@ -7,10 +8,9 @@ import test from './test';
 import popup from './components/popup';
 
 documentReady(() => {
+  AOS.init();
   lazyImages();
   scrollTo();
   test();
   popup();
 });
-
-
