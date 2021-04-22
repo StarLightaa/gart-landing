8919dd8e04bcce0cebe328417ccd18ae71de1072
123 <qeqw@eqwdas.com>
2021-04-22 11:22:15 +0300

    hover effects, phone mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import 'focus-visible';
 import AOS from 'aos';
+import IMask from 'imask';
 import lazyImages from './modules/lazyImages';
 import scrollTo from './modules/scrollTo';
 import documentReady from './helpers/documentReady';
@@ -13,4 +14,13 @@ documentReady(() => {
   scrollTo();
   test();
   popup();
+
+  const phone = document.querySelectorAll('.form__field--phone');
+  phone.forEach(el => {
+    const phoneMask = IMask(el, {
+      mask: '+{7}(000)000-00-00',
+      lazy: true,  // make placeholder always visible
+      // placeholderChar: '#'     // defaults to '_'
+    });
+  });
 });
